26f478bb1d16a7bce388742254cd7e1863ee0ebf
Visal Prom <visal@visals-macbook-pro.local>
2018-06-27 13:48:23 +0700

    gameplay modification

--- ARMajic/GameViewController.swift
+++ ARMajic/GameViewController.swift
@@ -12,7 +12,9 @@ class GameViewController: UIViewController {
     var objpizza = SCNNode()
     let arView: ARSCNView = {
         let view = ARSCNView()
-        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // no need this lines as it we used fillSuperView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
@@ -26,11 +28,26 @@ class GameViewController: UIViewController {
     
         return button
     }()
-    
     @objc func handlePlusButtonTaped(){
         print("Tapped on plus button")
         addbox()
         
+        
+    }
+    
+    var minusButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "minus"), for: .normal)
+        button.addTarget(self, action: #selector(handleMinusButtonTaped), for: .touchUpInside)
+        button.layer.zPosition = 1
+        button.imageView?.contentMode = .scaleAspectFill
+        
+        return button
+    }()
+    
+    @objc func handleMinusButtonTaped(){
+        print("Tapped on minus button")
+        removeAllBoxes()
         
     }
     
@@ -46,7 +63,7 @@ class GameViewController: UIViewController {
         arView.session.run(configuration, options: []) // left option blank
         // debug option
         arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-        arView.autoenablesDefaultLighting = true // auto add light to scene to make edges of box
+//        arView.autoenablesDefaultLighting = true // auto add light to scene to make edges of box
     }
     
     override var prefersStatusBarHidden: Bool{
@@ -60,6 +77,9 @@ class GameViewController: UIViewController {
         
         view.addSubview(plusButton)
         plusButton.anchor(nil, left: view.safeAreaLayoutGuide.leftAnchor , bottom: view.safeAreaLayoutGuide.bottomAnchor, right:nil, topConstant:0, leftConstant: 24 , bottomConstant:12 , rightConstant:0, widthConstant: 60, heightConstant: 60 )
+        
+        view.addSubview(minusButton)
+        minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant:0, leftConstant: 0 , bottomConstant:5 , rightConstant:24, widthConstant: 85, heightConstant: 85)
         
         //first method
 //        arView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -76,8 +96,10 @@ class GameViewController: UIViewController {
         //add box
         let boxNode = SCNNode()
         boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.002) // chamferRadius can extend the edges round
-        boxNode.position = SCNVector3(0,0,-0.1) // x,y,z ex: z = -0.3 = 30 centimeters
+        boxNode.position = SCNVector3(0,0,-0.2) // x,y,z ex: z = -0.3 = 30 centimeters
         boxNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "water") // can add materials or img/ UIColor
+        boxNode.name = "box"
+        
         arView.scene.rootNode.addChildNode(boxNode)
         
         
@@ -93,5 +115,13 @@ class GameViewController: UIViewController {
 //        node.constraints = [SCNBillboardConstraint()]
 //        return node
 //    }
+    func removeAllBoxes(){
+        arView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == "box"{
+                node.removeFromParentNode()
+            }
+         
+        }
+    }
     
 }
